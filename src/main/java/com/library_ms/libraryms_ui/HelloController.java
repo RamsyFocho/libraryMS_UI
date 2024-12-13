37d870a369a6de32a0ffac45a515c47535836f32
Ramsy <fochomukomr@gmail.com>
2024-12-13 12:27:11 +0100

    login and resgistration

---
+++ src/main/java/com/library_ms/libraryms_ui/HelloController.java
@@ -0,0 +1,14 @@
+package com.library_ms.libraryms_ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
